5b3c8ce50c607a948ac3860de2bbab1da068b6d4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-28 18:06:02 +0800

    create service user

---
+++ src/modules/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from '../controllers/app.controller';
+import { AppService } from '../service/app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModel } from 'src/models/user.model';
+import { UserModule } from './user/user.module';
+require('dotenv').config();
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'database', // Ganti 'localhost' dengan 'database'
+      port: 5432, // Pastikan port ini sesuai dengan yang ditetapkan di docker-compose
+      username: process.env.DB_USERNAME, // Gunakan variabel dari .env jika perlu
+      password: process.env.DB_PASSWORD, // Gunakan variabel dari .env jika perlu
+      database: process.env.DB_DATABASE, // Gunakan variabel dari .env jika perlu
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
