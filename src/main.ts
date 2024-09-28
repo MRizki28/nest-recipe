899fa52620f1b2aa66c70323f26a1041cc4af67f
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-28 18:05:34 +0800

    create service user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (validationError: ValidationError[] = []) => {
+      return new UnprocessableEntityException({
+        status: 'not validate',
+        message: validationError.map((error) => ({
+          field: error.property,
+          error: Object.values(error.constraints).join(', '),
+        })),
+      });
+    }
+  }
+  ));
+
+  app.use(bodyParser.json());
   await app.listen(3333);
 }
 bootstrap();
