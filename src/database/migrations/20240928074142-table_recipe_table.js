a644fedf90bae80a9120d1d07e61bc6877d8e75e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-09-28 15:48:54 +0800

    setup sequelize and docker

---
+++ src/database/migrations/20240928074142-table_recipe_table.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('recipe', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      name_recipe: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.ENUM('breakfast', 'dinner'),
+        allowNull: false,
+      },
+      dificulty: {
+        type: Sequelize.ENUM('easy', 'medium', 'hard'),
+        allowNull: false,
+      },
+      prep_time: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      cook_time: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      serving: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      img_recipe: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('recipe');
+  }
+};
