7e859653761d58b445cc5266d0b8c38ee7e1fa7c
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-03 11:46:48 +0800

    update validation

--- src/dto/user/user.dto.ts
+++ src/dto/user/user.dto.ts
@@ -1,9 +1,12 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, Validate } from "class-validator";
+import { IsEmailUnique } from "../IsEmailUniqueDecoration";
+import { UniqueEmail } from "src/config/validator/UniqueEmail";
 
 export class UserDto{
     @IsNotEmpty()
     name: string;
     @IsNotEmpty()
+    @Validate(UniqueEmail)
     email: string;
     @IsNotEmpty()
     password: string;
