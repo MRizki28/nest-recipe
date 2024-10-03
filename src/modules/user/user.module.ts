258215c3d1104fdc029b471c2f1e5354d2c8bf12
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-03 10:39:21 +0800

    setup JWT

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { JwtStrategy } from 'src/config/jwt/jwt.strategy';
 import { UserController } from 'src/controllers/user/user.controller';
 import { UserModel } from 'src/models/user.model';
 import { UserService } from 'src/service/user/user.service';
 
 @Module({
-    imports: [SequelizeModule.forFeature([UserModel])],
-    providers: [UserService],
+    imports: [SequelizeModule.forFeature([UserModel]),
+    PassportModule,
+    JwtModule.register({
+        global: true,
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '1d' }
+    })
+    ],
+    providers: [UserService, JwtStrategy],
     controllers: [UserController],
 })
-export class UserModule {}
+export class UserModule { }
