d8477be49c97de950e38b985e91a0ab170639db1
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-04 18:24:24 +0800

    add upload img

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { JwtStrategy } from 'src/config/jwt/jwt.strategy';
+import { FileUpload } from 'src/config/validator/FIleUpload';
 import { UniqueEmail } from 'src/config/validator/UniqueEmail';
 import { UserController } from 'src/controllers/user/user.controller';
 import { UserModel } from 'src/models/user.model';
