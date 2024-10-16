bc2e1014fa6dea4ddad7ebbc1b2fdc5ec39d08b3
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-16 18:17:36 +0800

    update recipe

---
+++ src/models/recipe.model.ts
@@ -0,0 +1,83 @@
+import { Injectable } from "@nestjs/common";
+import { Column, CreatedAt, DataType, ForeignKey, Model, Table, UpdatedAt } from "sequelize-typescript";
+import { UserModel } from "./user.model";
+
+@Table({
+    tableName: 'recipe',
+})
+
+@Injectable()
+export class RecipeModel extends Model<RecipeModel> {
+    @Column({
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+    })
+    id: string
+
+    @ForeignKey(() => UserModel)
+    @Column({
+        type: DataType.UUID,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+    })
+    user_id: string
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    name_recipe: string
+
+    @Column({
+        type: DataType.TEXT,
+        allowNull: false,
+    })
+    description: string
+
+    @Column({
+        type: DataType.ENUM('breakfast', 'dinner'),
+        allowNull: false,
+    })
+    category: 'breakfast' | 'dinner'
+
+    @Column({
+        type: DataType.ENUM('easy', 'medium', 'hard'),
+        allowNull: false,
+    })
+    difficulty: 'easy' | 'medium' | 'hard'
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    prep_time: number
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+
+    cook_time: number
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    serving: number
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    img_recipe: string
+
+    @CreatedAt
+    created_at: Date;
+
+    @UpdatedAt
+    updated_at: Date;
+
+}
