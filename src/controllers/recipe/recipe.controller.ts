8d3785b56d1dd53caf4760c6f8f9f115a4bd46d1
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-17 13:20:50 +0800

    add create recipe

--- src/controllers/recipe/recipe.controller.ts
+++ src/controllers/recipe/recipe.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UnprocessableEntityException, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/config/jwt/jwtAuth.guard';
+import { RecipeDto } from 'src/dto/recipe/recipe.dto';
+import { RecipeService } from 'src/service/recipe/recipe.service';
 
 @Controller('recipe')
-export class RecipeController {}
+export class RecipeController {
+    constructor(
+        private readonly recipeService: RecipeService
+    ) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post('/create')
+    @UseInterceptors(FileInterceptor('img_recipe'))
+    async createData(@Req() req, @Body() recipeDto: RecipeDto,@UploadedFile() img_recipe: Express.Multer.File): Promise<any> {
+        if(!img_recipe) {
+            throw new UnprocessableEntityException({
+                status: 'not validate',
+                message: [
+                    {
+                        field: 'img_recipe',
+                        error: 'Image Recipe is required',
+                    },
+                ],
+            });
+        }
+        return await this.recipeService.createData(req, recipeDto, img_recipe);
+    }
+}
