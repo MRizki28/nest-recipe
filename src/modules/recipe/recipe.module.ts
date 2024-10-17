b938ebec3646e807a8cdd1e016204542f5039cbf
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-17 13:21:06 +0800

    add create recipe

--- src/modules/recipe/recipe.module.ts
+++ src/modules/recipe/recipe.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { RecipeController } from 'src/controllers/recipe/recipe.controller';
 import { RecipeModel } from 'src/models/recipe.model';
-import { RecipeService } from 'src/services/recipe/recipe.service';
+import { UserModel } from 'src/models/user.model';
+import { RecipeService } from 'src/service/recipe/recipe.service';
 
 @Module({
-    imports: [SequelizeModule.forFeature([RecipeModel])],
+    imports: [SequelizeModule.forFeature([RecipeModel, UserModel])],
     providers: [RecipeService],
     controllers: [RecipeController],
 })
