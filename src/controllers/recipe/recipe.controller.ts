3d071dd779bf1d44b81bd5f193ce7a4aa42c93b3
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-10-18 09:56:20 +0800

    feat(recipe): add new function for update and delete

--- src/controllers/recipe/recipe.controller.ts
+++ src/controllers/recipe/recipe.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UnprocessableEntityException, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Req, UnprocessableEntityException, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/config/jwt/jwtAuth.guard';
 import { RecipeDto } from 'src/dto/recipe/recipe.dto';
@@ -26,5 +26,24 @@ export class RecipeController {
             });
         }
         return await this.recipeService.createData(req, recipeDto, img_recipe);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('/get/:id')
+    async getDataById(@Param('id') id: string): Promise<any> {
+        return await this.recipeService.getDataById(id);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('/update/:id')
+    @UseInterceptors(FileInterceptor('img_recipe'))
+    async updateData(@Param('id') id: string, @Body() recipeDto: RecipeDto, @UploadedFile() img_recipe: Express.Multer.File): Promise<any> {
+        return await this.recipeService.updateData(id, recipeDto, img_recipe);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete('/delete/:id')
+    async deleteData(@Param('id') id: string): Promise<any> {
+        return await this.recipeService.deleteData(id);
     }
 }
